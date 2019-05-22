b71f334c2836ee92de3dfd4a605cc1d6a048d5d9
alex-wdmg <wdmg.com.ua@gmail.com>
2019-05-22 17:19:35 +0300

    Added Bootstrap

---
+++ Bootstrap.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace wdmg\activity;
+
+/**
+ * @author          Alexsander Vyshnyvetskyy <alex.vyshnyvetskyy@gmail.com>
+ * @copyright       Copyright (c) 2019 W.D.M.Group, Ukraine
+ * @license         https://opensource.org/licenses/MIT Massachusetts Institute of Technology (MIT) License
+ */
+
+use yii\base\BootstrapInterface;
+use Yii;
+
+
+class Bootstrap implements BootstrapInterface
+{
+    public function bootstrap($app)
+    {
+        // Get the module instance
+        $module = Yii::$app->getModule('activity');
+
+        // Get URL path prefix if exist
+        $prefix = (isset($module->routePrefix) ? $module->routePrefix . '/' : '');
+
+        // Add module URL rules
+        $app->getUrlManager()->addRules(
+            [
+                $prefix . '<module:activity>/' => '<module>/activity/index',
+                $prefix . '<module:activity>/<controller:\w+>/' => '<module>/<controller>',
+                $prefix . '<module:activity>/<controller:\w+>/<action:\w+>' => '<module>/<controller>/<action>',
+                [
+                    'pattern' => $prefix . '<module:activity>/',
+                    'route' => '<module>/activity/index',
+                    'suffix' => '',
+                ], [
+                    'pattern' => $prefix . '<module:activity>/<controller:\w+>/',
+                    'route' => '<module>/<controller>',
+                    'suffix' => '',
+                ], [
+                    'pattern' => $prefix . '<module:activity>/<controller:\w+>/<action:\w+>',
+                    'route' => '<module>/<controller>/<action>',
+                    'suffix' => '',
+                ],
+            ],
+            true
+        );
+    }
+}
