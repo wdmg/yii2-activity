80960ef5c68b997285db6b6c3805efac4b9df8f2
Alexsander Vyshnyvetskyy <wdmg.com.ua@gmail.com>
2023-06-24 12:22:34 +0300

    Save autoupdate to user options

--- controllers/ListController.php
+++ controllers/ListController.php
@@ -10,6 +10,7 @@ use yii\data\ActiveDataProvider;
 use yii\filters\VerbFilter;
 use yii\filters\AccessControl;
 use wdmg\activity\models\Activity;
+use wdmg\activity\models\ActivitySearch;
 
 /**
  * ListController implements the CRUD actions for Activity model.
@@ -63,24 +64,63 @@ class ListController extends Controller
      */
     public function actionIndex()
     {
-        $model = new Activity();
-        $activity = new ActiveDataProvider([
-            'query' => $model::find(),
-            'pagination' => [
-                'pageSize' => 20,
-            ],
-            'sort' => [
-                'defaultOrder' => [
-                    'id' => SORT_DESC,
-                    'created_at' => SORT_ASC,
-                ]
-            ],
-        ]);
-        return $this->render('index', [
-            'activity' => $activity,
-            'model' => $model
-        ]);
+
+		// Set autoupdate list flag by default
+	    $this->view->params['auto-update'] = (bool)$this->module->autoupdateList;
+
+	    // Set autoupdate list flag by user options
+		if (!Yii::$app->user->isGuest && class_exists('\wdmg\users\models\Users')) {
+		    $user_options = \wdmg\users\models\Users::getOptions('activity', []);
+
+		    if (isset($user_options['autoupdate']))
+			    $this->view->params['auto-update'] = (bool)$user_options['autoupdate'];
+
+	    }
+
+	    $searchModel = new ActivitySearch();
+	    $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+	    return $this->render('index', [
+		    'searchModel' => $searchModel,
+		    'dataProvider' => $dataProvider,
+		    'module' => $this->module
+	    ]);
     }
+
+	/**
+	 * Add/remove from favourites
+	 *
+	 * @return Response
+	 */
+	public function actionAutoupdate()
+	{
+
+		\Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+
+		if (!Yii::$app->user->isGuest && class_exists('\wdmg\users\models\Users')) {
+
+			$autoupdate = \Yii::$app->request->post('autoupdate', null);
+			if (isset($autoupdate)) {
+
+				$user_options = \wdmg\users\models\Users::getOptions('activity', []);
+				if (!is_array($user_options))
+					$user_options = [];
+
+				$is_autoupdate = !($autoupdate == "false");
+				$user_options = array_merge($user_options, [
+					'autoupdate' => $is_autoupdate
+				]);
+
+				\wdmg\users\models\Users::setOptions(['activity' => [
+					'autoupdate' => $is_autoupdate
+				]], false);
+
+				return ['success' => true, 'autoupdate' => $is_autoupdate];
+			}
+		}
+
+		return ['success' => false];
+	}
 
     /**
      * Finds the Option model based on its primary key value.
